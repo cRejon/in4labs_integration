1333bfae4751056e13f65bb25bb996b26717b1ca
cRejon <carlosrejongomez@gmail.com>
2025-03-18 13:45:53 +0100

    Change position for boards Sensor and LCD

---
+++ in4labs_integration_app/config.py
@@ -0,0 +1,21 @@
+# Boards configuration
+boards_config = {
+    'Board_1':{
+        'name':'Sensor',
+        'model':'Arduino Nano ESP32',
+        'fqbn':'arduino:esp32:nano_nora',
+        'usb_port':'1',
+    },
+    'Board_2':{
+        'name':'LCD',
+        'model':'Arduino Nano ESP32',
+        'fqbn':'arduino:esp32:nano_nora',
+        'usb_port':'2',
+    },
+    'Board_3':{
+        'name':'Fan',
+        'model':'Arduino Nano ESP32',
+        'fqbn':'arduino:esp32:nano_nora',
+        'usb_port':'3',
+    }
+}
