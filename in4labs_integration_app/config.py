8febcad011dfb9813e17e84babd88a3ab53f83fc
cRejon <carlosrejongomez@gmail.com>
2025-08-12 13:27:19 +0200

    Refactor application structure and configuration to use a reverse proxy

--- in4labs_integration_app/config.py
+++ in4labs_integration_app/config.py
@@ -1,21 +1,39 @@
-# Boards configuration
-boards_config = {
-    'Board_1':{
-        'name':'Sensor',
-        'model':'Arduino Nano ESP32',
-        'fqbn':'arduino:esp32:nano_nora',
-        'usb_port':'1',
-    },
-    'Board_2':{
-        'name':'LCD',
-        'model':'Arduino Nano ESP32',
-        'fqbn':'arduino:esp32:nano_nora',
-        'usb_port':'2',
-    },
-    'Board_3':{
-        'name':'Fan',
-        'model':'Arduino Nano ESP32',
-        'fqbn':'arduino:esp32:nano_nora',
-        'usb_port':'3',
+import os
+
+
+class Config(object):
+    # Boards configuration
+    boards_config = {
+        'Board_1':{
+            'name':'Sensor',
+            'model':'Arduino Nano ESP32', 
+            'fqbn':'arduino:esp32:nano_nora', 
+            'usb_port':'1',
+        },
+        'Board_2':{
+            'name':'LCD',
+            'model':'Arduino Nano ESP32',
+            'fqbn':'arduino:esp32:nano_nora',
+            'usb_port':'2',
+        },
+        'Board_3':{
+            'name':'Fan',
+            'model':'Arduino Nano ESP32',
+            'fqbn':'arduino:esp32:nano_nora',
+            'usb_port':'3',
+        }
     }
-}
+
+    # Docker environment variables
+    server_name = os.environ.get('SERVER_NAME')
+    lab_name = os.environ.get('LAB_NAME')
+    user_email = os.environ.get('USER_EMAIL') 
+    end_time = os.environ.get('END_TIME') 
+    cam_url = os.environ.get('CAM_URL') 
+
+    # Flask environment variable needed for session management
+    flask_config = {
+        # Use as secret key the user email + the end time of the session 
+        'SECRET_KEY': user_email + end_time,
+        'SESSION_COOKIE_NAME': user_email + end_time,
+    }
